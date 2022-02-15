e11647f17bfd402a94841151568d0b2b1dd86961
Hieu Nguyen Duy <hieuduy1751@gmail.com>
2022-02-15 10:50:23 +0700

    add cat class

---
+++ cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+	private String name;
+	private int age;
+
+	public Cat() {}
+	
+	public Cat(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void meow() {
+		System.out.print("meow");
+	}
+}
